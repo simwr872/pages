626c17529ab601e7961b53f61fe80c71dfa52683
Simon Wrede <simwr872@student.liu.se>
2020-11-03 17:58:04 +0100

    Svelte template

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import svelte from 'rollup-plugin-svelte';
+import { terser } from 'rollup-plugin-terser';
+import sveltePreprocess from 'svelte-preprocess';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+    input: 'src/main.ts',
+    output: {
+        sourcemap: !production,
+        format: 'iife',
+        name: 'app',
+        file: 'public/build/bundle.js'
+    },
+    plugins: [
+        svelte({
+            dev: !production,
+            css: css => {
+                css.write('bundle.css', !production);
+            },
+            preprocess: sveltePreprocess()
+        }),
+        resolve({
+            browser: true,
+            dedupe: ['svelte']
+        }),
+        commonjs(),
+        typescript({
+            sourceMap: !production,
+            inlineSources: !production
+        }),
+        production && terser()
+    ],
+    watch: {
+        clearScreen: false
+    }
+};
